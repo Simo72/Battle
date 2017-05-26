6326000f9d2b4b1b7b6003454f3789848e4e727c
Elena Morton <elmo@Elenas-MacBook-Pro.local>
2017-05-26 10:15:16 +0100

    Game accepting 2 players.

--- app.rb
+++ app.rb
@@ -3,27 +3,30 @@ require './lib/player'
 require './lib/game'
 
 class Battle < Sinatra::Base
-  
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player1])
-    $player_2 = Player.new(params[:player2])
+    # $player_1 = Player.new(params[:player1])
+    # $player_2 = Player.new(params[:player2])
+    $game = Game.new(Player.new(params[:player1]), Player.new(params[:player2]))
     redirect to '/play'
   end
 
   get '/play' do
-    @player1 = $player_1
-    @player2 = $player_2
+    @game = $game
+    @player1 = @game.player1
+    @player2 = @game.player2
     erb :play
   end
 
   get '/attack' do
-    @player1 = $player_1
-    @player2 = $player_2
-    Game.new.attack(@player2)
+    @game = $game
+    @player1 = @game.player1
+    @player2 = @game.player2
+    @game.attack(@player2)
     erb :attack
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,20 @@
 require './lib/game'
 
 describe Game do
-  subject(:game) {described_class.new}
-  let(:player1) {double :player}
-  let(:player2) {double :player}
+  subject(:game) { described_class.new(player1, player2) }
+  let(:player1) { double :player }
+  let(:player2) { double :player }
+
+  describe '#initialize accepts 2 players' do
+    it 'accepts player 1' do
+      expect(game.player1).to eq player1
+    end
+
+    it 'accepts player 2' do
+      expect(game.player2).to eq player2
+    end
+
+  end
 
   describe '#attack' do
     it 'damages the player' do
