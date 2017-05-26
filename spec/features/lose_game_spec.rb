d97bbc486b86b4f8372dac30a6bdc1072a037225
IAN SIMPSON <ianconradsimpson@gmail.com>
2017-05-26 15:22:46 +0100

    Added loser

---
+++ spec/features/lose_game_spec.rb
@@ -0,0 +1,30 @@
+# As a Player,
+# So I can Lose a game of Battle,
+# I want to see a 'Lose' message if I reach 0HP first
+
+feature 'Lose game' do
+  scenario 'Player 2 loses game' do
+    sign_in_and_play
+    click_button('Attack')
+    click_link("OK")
+    click_button('Attack')
+    click_link("OK")
+    click_button('Attack')
+    click_link("OK")
+    click_button('Attack')
+    click_link("OK")
+    click_button('Attack')
+    click_link("OK")
+    click_button('Attack')
+    click_link("OK")
+    click_button('Attack')
+    click_link("OK")
+    click_button('Attack')
+    click_link("OK")
+    click_button('Attack')
+    click_link("OK")
+    click_button('Attack')
+    click_link("OK")
+expect(page).to have_content "You lose Sam 0 points!!!"
+  end
+end
